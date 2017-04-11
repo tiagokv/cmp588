3eab947c6c417a331dc56cc0b0a2c2e729721839
Tiago Kramer Vieira <tiagokv@hotmail.com>
2017-04-11 20:37:04 -0300

    Enabling vertex measures

--- dijkstra/measure/dijkstra_measure.cpp
+++ dijkstra/measure/dijkstra_measure.cpp
@@ -14,76 +14,90 @@ struct vert_random{
 	vector<unsigned int> vertexes;
 };
 
-void build_graph(Graph& graph, int max_vertexes, int quantity_edges){
+void build_graph(bool vary_edge, Graph& graph, int max_vertexes, int quantity_edges){
 	graph.resize(max_vertexes);
 	uniform_int_distribution<unsigned int> rand_vertex(0, max_vertexes-1);
 	uniform_int_distribution<unsigned int> rand_weight(0, 1000);
 
-	std::vector<vert_random> v;
+	// if( vary_edge ){
+		std::vector<vert_random> v;
 
-	//Yes, this is not good for memory, but is temporary
-	for (unsigned int i = 0; i < max_vertexes; ++i)
-	{
-		vert_random vert;
-		vert.index = i;
-		//vert.vertexes.resize(max_vertexes);
-		for (unsigned int j = 0; j < max_vertexes; ++j)
+		//Yes, this is not good for memory, but is temporary to build the graph
+		for (unsigned int i = 0; i < max_vertexes; ++i)
 		{
-			if( i != j ){
-				vert.vertexes.push_back(j);
+			vert_random vert;
+			vert.index = i;
+			//vert.vertexes.resize(max_vertexes);
+			for (unsigned int j = 0; j < max_vertexes; ++j)
+			{
+				if( i != j ){
+					vert.vertexes.push_back(j);
+				}
+			}
+			v.push_back(vert);
+		}
+
+		//connect all vertexes
+		while(!v.empty() && quantity_edges > 0){
+			uniform_int_distribution<size_t> rand_vertex(0, v.size()-1);
+			size_t from_index = rand_vertex(mt);
+
+			uniform_int_distribution<size_t> rand_vertex_to(0, v[from_index].vertexes.size()-1);
+			
+			size_t to_index = rand_vertex_to(mt);
+
+			graph.connect(v[from_index].index, v[from_index].vertexes[to_index], rand_weight(mt));
+			quantity_edges--;
+
+			v[from_index].vertexes.erase(begin(v[from_index].vertexes) + to_index);
+			if( v[from_index].vertexes.empty() ){
+				v.erase(begin(v) + from_index);
 			}
 		}
-		v.push_back(vert);
-	}
 
-	//connect all vertexes
-	while(!v.empty() && quantity_edges > 0){
-		uniform_int_distribution<size_t> rand_vertex(0, v.size()-1);
-		size_t from_index = rand_vertex(mt);
+		// sanity check
+		//cout << "terminou com " << quantity_edges << " e com " << v.size() << " sobrando" << endl;
+	/*}else{
+		int connected = 0;
+		while(quantity_edges-- && connected <= pow(max_vertexes,2) ){
+			unsigned int from = rand_vertex(mt);
+			unsigned int to = rand_vertex(mt);
 
-		uniform_int_distribution<size_t> rand_vertex_to(0, v[from_index].vertexes.size()-1);
-		
-		size_t to_index = rand_vertex_to(mt);
-
-		graph.connect(v[from_index].index, v[from_index].vertexes[to_index], rand_weight(mt));
-		quantity_edges--;
-
-		v[from_index].vertexes.erase(begin(v[from_index].vertexes) + to_index);
-		if( v[from_index].vertexes.empty() ){
-			v.erase(begin(v) + from_index);
+			int tentatives = 0;
+			while(from == to || graph.are_connected(from,to)){
+				if( tentatives > 100 ){
+					auto adj = graph.adjacents(from);
+					for (int i = 0; i < max_vertexes; ++i)
+					{
+						
+					}
+				}
+				to = rand_vertex(mt);
+				tentatives++;
+			}
+			graph.connect(from, to, rand_weight(mt));
+			connected++;
 		}
-	}
-
-	// sanity check
-	//cout << "terminou com " << quantity_edges << " e com " << v.size() << " sobrando" << endl;
-
-/*	while(quantity_edges--){
-		unsigned int from = rand_vertex(mt);
-		unsigned int to = rand_vertex(mt);
-		while(from == to || graph.are_connected(from,to)){
-			to = rand_vertex(mt);
-		}
-		graph.connect(from, to, rand_weight(mt));
 	}*/
 }
 
-int main(int argv, char** argc){
-
-	const int max_vertexes = pow(2, 10);
-	const int max_repetition = 30;
-	const int max_edges = 21;
-	const int min_edges = 10;
+void measure_dijkstra(bool vary_edges, int max_objs, int max_repetition, int min_var_obj, int max_var_obj){
 
     cout << "i I(i) D(i) U(i) T(i)" << endl;
 
-	uniform_int_distribution<unsigned int> rand_vertex(0, max_vertexes-1);
-
-	for(int i = min_edges; i < max_edges; i++){
-		int quantity_edges = pow(2, i);
+	for(int i = min_var_obj; i < max_var_obj; i++){
 
 		int repetition = max_repetition;
 		Graph g;
-		build_graph(g, max_vertexes, quantity_edges);
+
+		if( vary_edges ){
+			build_graph(vary_edges, g, max_objs, pow(2, i));
+		}else{
+			build_graph(vary_edges, g, pow(2, i), max_objs);
+		}
+
+		uniform_int_distribution<unsigned int> rand_vertex(0, g.number_vertexes()-1);
+
 		unsigned int stop_vertex = rand_vertex(mt);
 
 		unsigned int num_pops_max = 0, num_pushes_max = 0, num_updates_max = 0;
@@ -109,6 +123,29 @@ int main(int argv, char** argc){
 				  << " " << total.count() << endl;
 	}
 	
+}
+
+int main(int argv, char** argc){
+
+	// const int max_vertexes = pow(2, 10);
+	// const int max_repetition = 30;
+	// const int max_edges = 21;
+	// const int min_edges = 10;
+
+	bool vary_edges = argc[1][0] == 'e';
+	int max_objs = atoi(argc[2]);
+	int max_repetition = atoi(argc[3]);
+	int min_var_obj = atoi(argc[4]);
+	int max_var_obj = atoi(argc[5]);
+
+	cerr << "Enter following parameters: " << endl;
+	cerr << "Vary edges: " << boolalpha << vary_edges << endl;
+	cerr << "Max objs: " << max_objs << endl;
+	cerr << "Max repetition: " << max_repetition << endl;
+	cerr << "Min var obj: " << min_var_obj << endl;
+	cerr << "Max var obj: " << max_var_obj << endl;
+
+	measure_dijkstra(vary_edges, max_objs, max_repetition, min_var_obj, max_var_obj);
 
 	return 0;
 }
--- dijkstra/src/Dijkstra.hpp
+++ dijkstra/src/Dijkstra.hpp
@@ -1,3 +1,8 @@
+#ifndef DIJKSTRA_HPP_
+#define DIJKSTRA_HPP_
+#include <memory>
+#include <iostream>
+
 #include "Graph.hpp"
 
 class Vertex
@@ -10,6 +15,16 @@ public:
 	Vertex(unsigned int index, unsigned int distance_to, unsigned int came_from ):index(index), 
 																				  distance_to(distance_to), 
 																				  came_from(came_from){};
+	virtual bool operator<(const Vertex& rhs){
+		std::cout << "compared" << std::endl; 
+		return this->distance_to < rhs.distance_to;
+	}
 };
 
-int shortest_path(const Graph& graph, unsigned int from, unsigned int to);
+int shortest_path(const Graph& graph, unsigned int from, unsigned int to, 
+				  unsigned int& num_pops, unsigned int& num_pushes, unsigned int& num_updates);
+
+
+#endif
+
+
